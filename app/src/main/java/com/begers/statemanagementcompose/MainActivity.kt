71f50283c70a9a42dbc2928422662105ad9b2d34
bekirgeris <bekirgeris@marun.edu.tr>
2021-12-07 23:48:16 +0300

    refactor

--- app/src/main/java/com/begers/statemanagementcompose/MainActivity.kt
+++ app/src/main/java/com/begers/statemanagementcompose/MainActivity.kt
@@ -3,9 +3,8 @@ package com.begers.statemanagementcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -13,6 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.begers.statemanagementcompose.ui.theme.StateManagementComposeTheme
 
@@ -31,8 +31,18 @@ fun MainScreen() {
         Column(modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally) {
+            
             Text(text = "Hello",
             fontSize = 26.sp)
+            
+            Spacer(modifier = Modifier.padding(7.dp))
+            
+            Button(onClick = {
+                println("clicked")
+            }) {
+                Text(text = "Butoon")
+                Text(text = "Test")
+            }
         }
     }
 
