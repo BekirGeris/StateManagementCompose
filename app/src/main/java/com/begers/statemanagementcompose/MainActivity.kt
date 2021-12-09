c54741055e77a80a61312779aae7f46a2ac146d6
bekirgeris <bekirgeris@marun.edu.tr>
2021-12-09 16:03:40 +0300

    +remembering the text

--- app/src/main/java/com/begers/statemanagementcompose/MainActivity.kt
+++ app/src/main/java/com/begers/statemanagementcompose/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -40,26 +42,34 @@ fun MainScreen() {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally) {
 
-            var myString = "Android Compose"
+            val myString = remember {
+                mutableStateOf("Android Compose")
+            }
+            val myInt = mutableStateOf(5)
 
-            TextField(value = TextFieldValue(text = myString),
+            TextField(
+                value = myString.value,
                 onValueChange = {
-                    myString = it.text
-                    println(myString)
+                    myString.value = it
+                    println(myString.value)
                 })
 
-            var textString = "Hello"
+            var textString = remember {
+                mutableStateOf("Hello")
+            }
 
             Spacer(modifier = Modifier.padding(7.dp))
 
-            Text(text = textString,
-            fontSize = 26.sp)
+            Text(
+                text = textString.value,
+                fontSize = 26.sp)
             
             Spacer(modifier = Modifier.padding(7.dp))
             
-            Button(onClick = {
+            Button(
+                onClick = {
                 println(textString)
-                textString = "Merhaba"
+                textString.value = "Merhaba"
             }) {
                 Text(text = "Butoon")
                 Text(text = "Test")
@@ -67,13 +77,15 @@ fun MainScreen() {
 
             Spacer(modifier = Modifier.padding(7.dp))
 
-            Image(bitmap = ImageBitmap.imageResource(id = R.drawable.images),
+            Image(
+                bitmap = ImageBitmap.imageResource(id = R.drawable.images),
                 contentDescription = null,
                 modifier = Modifier.padding(5.dp))
 
             Spacer(modifier = Modifier.padding(7.dp))
 
-            Image(painter = ColorPainter(Color.DarkGray),
+            Image(
+                painter = ColorPainter(Color.DarkGray),
                 contentDescription = null,
                 modifier = Modifier.size(20.dp, 20.dp))
 
